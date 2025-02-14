eed31dd6d70ed01537393522737dae973a66ff40
gimye <gimyedev@gmail.com>
2025-02-14 17:35:17 +0900

    Initial commit

---
+++ src/main/java/_dmp/setd_server/SetdServerApplication.java
@@ -0,0 +1,13 @@
+package _dmp.setd_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SetdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SetdServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/_dmp/setd_server/SetdServerApplicationTests.java
@@ -0,0 +1,13 @@
+package _dmp.setd_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SetdServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
